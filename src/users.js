c4c36494ef27bff846b8e3d9f4618f32a87bff32
Andres Arias <andres.arias12@gmail.com>
2017-09-12 16:36:40 -0600

    Varas que pide aaron

--- src/users.js
+++ src/users.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser");
 // ======== DATABASE CONNECTIONS ========
 var dbSanJose;
 var dbAlajuela;
+var dbHeredia;
 
 MongoClient.connect(driver.SanJose, function(err, db) {
     if (err) { return console.dir(err) }
@@ -16,6 +17,11 @@ MongoClient.connect(driver.SanJose, function(err, db) {
 MongoClient.connect(driver.Alajuela, function(err, db) {
     if (err) { return console.dir(err) }
     dbAlajuela = db;
+});
+
+MongoClient.connect(driver.Heredia, function(err, db) {
+    if (err) { return console.dir(err) }
+    dbHeredia = db;
 });
 
 // ======== BODY PARSER INIT ========
@@ -67,44 +73,36 @@ given.
 */
 router.post('/login', function(req, res) {
     var loginInfo = req.body;
-
-    if (loginInfo.Store == "SJ") { // Checks inside the SJ database.
-        dbSanJose.collection("Users").find({"Name" : loginInfo.Name,
-                                            "Password" : loginInfo.Password,
-                                            "Store" : loginInfo.Store})
-                                            .toArray(
-            function(error, result) { // No user found with the details given.
-                if (result.length == 0) {
-                    res.json({"Result" : 0});
-                } else { // User found, return success indicator.
-                    res.json({"Result" : 1});
-                }
+    console.log(loginInfo);
+    dbSanJose.collection("Users").find({"Name" : loginInfo.Name,
+                                        "Password" : loginInfo.Password})
+                                        .toArray(
+        function(error, result) {
+            if (result.length == 0) { // No user found with the details given.
+                dbAlajuela.collection("Users").find({"Name" : loginInfo.Name,
+                                                    "Password" : loginInfo.Password})
+                                                    .toArray(
+                    function(error, result) {
+                        if (result.length == 0) { // No user found with the details given.
+                            res.json({"Result" : 0});
+                        } else { // User found, return success indicator.
+                            res.json({"Result" : 1, "Name" : result[0].Name, "Store" : result[0].Store});
+                        }
+                    }
+                );
+            } else { // User found, return success indicator.
+                res.json({"Result" : 1, "Name" : result[0].Name, "Store" : result[0].Store});
             }
-        );
-    } else if (loginInfo.Store == "Alajuela") { // Checks inside the Alajuela database.
-        dbAlajuela.collection("Users").find({"Name" : loginInfo.Name,
-                                            "Password" : loginInfo.Password,
-                                            "Store" : loginInfo.Store})
-                                            .toArray(
-            function(error, result) {
-                if (result.length == 0) { // No user found with the details given.
-                    res.json({"Result" : 0});
-                } else { // User found, return success indicator.
-                    res.json({"Result" : 1});
-                }
-            }
-        );
-    } else { // The user is asking for a store that doesn't exists.
-        res.json({"Result" : 0});
-    }
+        }
+    );
 });
 
 /*
 Register function: Stores a new user in a given database.
 */
 router.post('/add', function(req, res) {
-    var regInfo = req.body;
-
+    var regInfo = req.query;
+    console.log(regInfo)
     if (regInfo.Store == "SJ") {
         dbSanJose.collection("Users").insert(regInfo);
         res.json({"Success" : 1, "Name" : regInfo.Name, "Store" : regInfo.Store});
@@ -115,5 +113,10 @@ router.post('/add', function(req, res) {
         res.json({"Success" : 0, "Name" : regInfo.Name, "Store" : regInfo.Store});
     }
 });
+
+router.post('/addClient', function(req, res) {
+    var custInfo = req.query;
+    console.log(cusInfo);
+})
 
 module.exports = router;
