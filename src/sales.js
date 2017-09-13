196f13826614dadfe507409d0051994003a9f2e4
Andres Arias <andres.arias12@gmail.com>
2017-09-13 01:36:06 -0600

    Agregado reporte de ventas por producto

--- src/sales.js
+++ src/sales.js
@@ -38,32 +38,29 @@ router.use(bodyParser.json());
 
 // ======== CALLBACK FUNCTIONS ========
 
-function retrieveData(idTienda, callback) {
+function retrieveProducts(idTienda, prodName, callback) {
     if (idTienda == 'Alajuela') {
         console.log('retrieving data in alajuela');
-        dbAlajuela.collection("Products").find({}).toArray(function(err, result) {
+        dbAlajuela.collection("Products").find({"name" : prodName}).toArray(function(err, result) {
             if (err) throw err;
-            console.log(result);
             callback(result);
         });
     } else if (idTienda == 'SJ') {
         console.log('retrieving data in alajuela');
-        dbSanJose.collection("Products").find({}).toArray(function(err, result) {
+        dbSanJose.collection("Products").find({"name" : prodName}).toArray(function(err, result) {
             if (err) throw err;
-            console.log(result);
             callback(result);
         });
     } else if (idTienda == "all") {
         var productsSJ;
         var productsAlajuela;
-        dbSanJose.collection("Products").find({}).toArray(function(err, result) {
+        dbSanJose.collection("Products").find({"name" : prodName}).toArray(function(err, result) {
             if (err) throw err;
             productsSJ = result;
-            dbAlajuela.collection("Products").find({}).toArray(function(err, result) {
+            dbAlajuela.collection("Products").find({"name" : prodName}).toArray(function(err, result) {
                 if (err) throw err;
                 productsAlajuela = result;
                 var total = productsSJ.concat(productsAlajuela);
-                console.log(total);
                 callback(total);
             });
         });
@@ -73,10 +70,46 @@ function retrieveData(idTienda, callback) {
 
 }
 
+function retrieveOrders(idTienda, callback) {
+    if (idTienda == 'Alajuela') {
+        dbAlajuela.collection("Orders").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    } else if (idTienda == 'SJ') {
+        dbSanJose.collection("Orders").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    } else if (idTienda == "all") {
+        var ordersSJ;
+        var ordersAlajuela;
+        dbSanJose.collection("Orders").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            ordersSJ = result;
+            dbAlajuela.collection("Orders").find({}).toArray(function(err, result) {
+                if (err) throw err;
+                ordersAlajuela = result;
+                var total = ordersSJ.concat(ordersAlajuela);
+                callback(total);
+            });
+        });
+    } else {
+
+    }
+
+}
+
+function byProductCallback(productName, callback) {
+    products = retrieveProducts("all", function(products) {
+
+    });
+}
+
 // ======== ROUTES ========
 
-router.get('/products/', function(req, res) {
-    data = retrieveData("all", function(data) {
+/*router.get('/products/', function(req, res) {
+    data = retrieveProducts("all", function(data) {
         response = {};
         response.data = data;
         res.json(response);
@@ -84,11 +117,32 @@ router.get('/products/', function(req, res) {
     });
 });
 
-router.get('/byProduct', function(req, res) {
+router.get('/orders/', function(req, res) {
+    data = retrieveOrders("all", function(data) {
+        response = {};
+        response.data = data;
+        res.json(response);
 
-    var products = retrieveData("all", function(data) {
-        res.send("Listo");
     });
-});
+});*/
+
+router.get('/report/:product', function(req, res) {
+    var productName = req.params.product;
+    data = retrieveOrders("all", function(data) {
+        var orderArray = data;
+        var totalSold = 0;
+        for (var i = 0 ; i < orderArray.length ; i++) {
+            var products = orderArray[i].products;
+            for (var j = 0 ; j < products.length ; j++) {
+                console.log(products[j]);
+                if (products[j].name == productName) {
+                    totalSold += products[j].price;
+                    console.log(totalSold);
+                }
+            }
+        }
+        res.json({"total" : totalSold});
+    });
+})
 
 module.exports = router;
